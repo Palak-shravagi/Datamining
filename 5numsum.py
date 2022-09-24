c74eaa4f09cf0bff375c9055279d63b347d8f126
Palak-shravagi <palakshravagi@gmail.com>
2022-09-24 15:48:16 +0530

    files added

---
+++ 5numsum.py
@@ -0,0 +1,79 @@
+
+import openpyxl
+import math
+
+
+def cal_median(array, l, r):
+    n = r+l
+    median = 0
+    if n % 2 == 0:
+        median = array[n//2]
+    else:
+        median1 = array[n//2]
+        median2 = array[(n//2) + 1]
+
+        median = (median1 + median2)/2
+    return median
+
+
+def five_no_summary(Arr, Ans):
+    min = Arr[0]
+    max = Arr[0]
+    for x in Arr:
+        if (x < min):
+            min = x
+    for x in Arr:
+        if (x > max):
+            max = x
+
+    Arr.sort()
+    n = len(Arr)
+    # Median
+    Med = cal_median(Arr, 0, n-1)
+    # Q1,Q3
+    Q1 = 0
+    Q3 = 0
+    if (n % 2 == 0):
+        Q1 = cal_median(Arr, 0, (n//2)-1)
+        Q3 = cal_median(Arr, n//2, n-1)
+    else:
+        Q1 = cal_median(Arr, 0, (n//2)-1)
+        Q3 = cal_median(Arr, (n+1)//2, n-1)
+    Ans.append(min)
+    Ans.append(Q1)
+    Ans.append(Med)
+    Ans.append(Q3)
+    Ans.append(max)
+
+
+wb = openpyxl.load_workbook(
+    "/home/palak/Documents/assignments/dm/datafile/numsum_.xlsx")
+sheet_obj = wb.active
+row = sheet_obj.max_row
+col = sheet_obj.max_column
+
+i = 1
+
+# for i in range(1,col+1):
+arr = []
+ans = []
+for j in range(2, row+1):
+    val = sheet_obj.cell(row=j, column=i)
+    arr.append(val.value)
+
+five_no_summary(arr, ans)
+i = 0
+j = 2
+
+arr1 = ["Min", "Quatrile1", "Median/Quatrile2", "Quatrile3", "Max"]
+
+for i in range(0, 5):
+    c0 = sheet_obj.cell(row=j, column=3)
+    c1 = sheet_obj.cell(row=j, column=4)
+
+    c0.value = arr1[i]
+    c1.value = ans[i]
+    i = i+1
+    j = j+1
+
+wb.save("/home/palak/Documents/assignments/dm/datafile/numsum_.xlsx")
---
+++ td_wt.py
@@ -0,0 +1,37 @@
+import pandas as pd
+df = pd.read_csv(
+    '/home/palak/Documents/assignments/dm/datafile/t&d_wt.csv', names=['A', 'B'])
+data = df.values.tolist()
+c1_tot = 0
+c2_tot = 0
+r1_tot = 0
+r2_tot = 0
+for i in range(2):
+    for j in range(2):
+        if (i == 0):
+            r1_tot = r1_tot+int(float(data[i][j]))
+        if (i == 1):
+            r2_tot = r2_tot+int(float(data[i][j]))
+        if (j == 0):
+            c1_tot = c1_tot+int(float(data[i][j]))
+        if (j == 1):
+            c2_tot = c2_tot+int(float(data[i][j]))
+print("Row 1 Total :", r1_tot)
+print("Row 2 Total :", r2_tot)
+print("Col 1 Total :", c1_tot)
+print("Col 2 Total :", c2_tot)
+print("")
+for i in range(2):
+    for j in range(2):
+        if (i == 0):
+            print("t_weight for data " +
+                  str(data[i][j]) + " is: ", float((data[i][j]/r1_tot) * 100))
+        if (i == 1):
+            print("t_weight for data " +
+                  str(data[i][j]) + " is: ", float((data[i][j]/r2_tot) * 100))
+        if (j == 0):
+            print("d_weight for data " +
+                  str(data[i][j]) + " is: ", float((data[i][j]/c1_tot) * 100))
+        if (j == 1):
+            print("d_weight for data " +
+                  str(data[i][j]) + " is: ", float((data[i][j]/c2_tot) * 100))
