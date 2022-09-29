7857d643bd12873215f038a76db8e4a62f6d4ca5
Palak-shravagi <palakshravagi@gmail.com>
2022-09-29 13:17:39 +0530

    changes added

--- corelation_coefficient/coefficient.py
+++ corelation_coefficient/coefficient.py
@@ -1,51 +1,41 @@
 import pandas
 
 data = pandas.read_csv("corelation_ip.csv")
+print(type(data))
 
-A = data['A'].tolist()
-B = data['B'].tolist()
-C = data['C'].tolist()
-D = data['D'].tolist()
 
-n = len(A)
+X = data['A'].tolist()
+# B = data['B'].tolist()
+# C = data['C'].tolist()
+# D = data['D'].tolist()
+
+n = len(X)
+# print(data.shape)
+# print(len(data.axes[1]))
+print("number of column present ", len(data.axes[1]))
+
+col1 = input("enter the index of column 1 : ")
+col2 = input("enter the index of column 2 : ")
+# A = df.iloc[col1,col1+1]
+# B = df.iloc[col2,col2+1]
+
+
+col1 = data.iloc[:, int(col1)-1]
+col2 = data.iloc[:, int(col2)-1]
+
+
 prob_A = 0
 prob_B = 0
 prob_A_B = 0
-prob_C = 0
-prob_B_C = 0
-prob_D = 0
-prob_C_D = 0
 
 for i in range(n):
-    if A[i] and B[i]:
+    if col1[i] and col2[i]:
         prob_A_B += 1
-    if A[i] == 1:
+    if col1[i] == 1:
         prob_A += 1
-    if B[i] == 1:
+    if col2[i] == 1:
         prob_B += 1
-
-
-for i in range(n):
-    if B[i] and C[i]:
-        prob_B_C += 1
-    if B[i] == 1:
-        prob_B += 1
-    if C[i] == 1:
-        prob_C += 1
-        
-
-for i in range(n):
-    if C[i] and D[i]:
-        prob_C_D += 1
-    if C[i] == 1:
-        prob_C += 1
-    if D[i] == 1:
-        prob_D += 1        
 
 ans = (prob_A_B)/(prob_A*prob_B)
-ans1 = (prob_B_C)/(prob_B*prob_C)
-ans2 = (prob_C_D)/(prob_C*prob_D)
 
-print("Corelation coefficient of column A and B ", ans)
-print("Corelation coefficient of column B and C ", ans1)
-print("Corelation coefficient of column C and D ", ans2)
+print("Corelation coefficient of columns is : ", ans)
